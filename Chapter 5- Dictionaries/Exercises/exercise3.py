4a05fda5c08a243066b23d834ebc597842d50aee
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:29:10 +0400

    Create exercise3.py
    exercise 3

---
+++ Chapter 5- Dictionaries/Exercises/exercise3.py
@@ -0,0 +1,17 @@
+glossary ={ 
+    "variable": "it is a container for storing data values.",
+    "function": "A block of reusable code that can perform a specific task.",
+    "loop": "Work through a collection of items,one at a time.",
+    "condition": "A statement that controls the flow of execution based on whether a certain condition is true or false.",
+    "method": "A function that is associated with an object and can be called on that object.",
+    "index": "An integer value representing a position in  a list.",
+    "float": "A number with a decimal point.",
+    "list": "A collection of items in particular order",
+    "dictionary": "A collection of key-value pairs",
+    "module": "A file containing Python definitions and statements",
+    }
+
+for key, value in glossary.items():
+    print(f"\nKey: {key}")
+    print(f"Value:{value}")
+
