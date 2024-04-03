e4b8e0010a92c0b5642aed7db7055642bad5c581
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:28:54 +0400

    Create exercise1.py
    exercise 1

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,11 @@
+person = { 
+    "first_name": "Sidra",
+    "last_name": "Islam",
+    "age": 23,
+    "city": "Pakistan"
+}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
