47ea0c10e89009bb756cc93da4907898185d33b8
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:27:09 +0400

    Create exercise3.py
    exercise 3

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,7 @@
+favourite_transports = ["car","motorcyle","train"]
+
+print("I love driving my audi"+favourite_transports[0]+"on rainy days.")
+
+print("I prefer going on a motorcycle"+ favourite_transports[1]+"to avoid congestion.")
+
+print("its fun and adventerous"+ favourite_transports[2]+"and it is environment friendly.")
