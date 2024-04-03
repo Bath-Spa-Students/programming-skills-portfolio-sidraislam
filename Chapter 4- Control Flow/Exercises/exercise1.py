0ca6a7085818830472f9dfb78ccfc8a8e49291fb
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:28:06 +0400

    Create exercise1.py
    exercise 1

---
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -0,0 +1,14 @@
+# PASSING VERSION
+
+alien_color = "green"
+
+if alien_color =="green":
+     print("you earned 5 points!")
+
+
+     # FAILING VERSION
+
+     alien_color = "yellow"
+
+     if alien_color == "green":
+          print("you earned 5")
