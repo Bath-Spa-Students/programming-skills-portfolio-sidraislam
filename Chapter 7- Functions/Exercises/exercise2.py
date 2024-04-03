2fe54a12674d4e5d638a88c72006cfcd1643e1fa
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:31:03 +0400

    Create exercise2.py
    exercise2

---
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -0,0 +1,5 @@
+def favourite_book(title):
+     """Display a message about someone's favourite book."""
+     print(title+"is one of my favourite books.")
+
+     favourite_book ('One Hundred Years of Solitude')
