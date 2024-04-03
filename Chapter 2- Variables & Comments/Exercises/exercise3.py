ae74e297a0576053cb22af8b69e5695d6e361189
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:26:23 +0400

    Create exercise3.py
    exercise 3

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,13 @@
+name = "\tSidra Islam\n"
+
+print("funny:")
+print(name)
+
+print("\nusing lstrip():")
+print(name.lstrip())
+
+print("\nusing rstrip():")
+print(name.rstrip())
+
+print("\nusing strip():")
+print(name.strip())
