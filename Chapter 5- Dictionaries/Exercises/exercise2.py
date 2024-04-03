e906d0a389abbaf1c1edeefb55d4cf015b3358f5
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:29:03 +0400

    Create exercise2.py
    exercise 2

---
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -0,0 +1,18 @@
+glossary = {
+    "variable": "it is a container for storing data values.",
+    "function": "A block of reusable code that can perform a specific task.",
+    "loop": "A control flow statement that allows code to be executed repeatedly.",
+    "condition": "A statement that controls the flow of execution based on whether a certain condition is true or false.",
+    "method": "A function that is associated with an object and can be called on that object."}
+
+word = 'variable'
+print ("\n"+ word.title()+": "+ glossary[word])
+
+word = 'function'
+print("\n"+ word.title() +": "+ glossary[word])
+
+word = 'loop'
+print("\n"+ word.title() +": "+ glossary[word])
+
+word = 'condition'
+print("\n"+ word.title() +": "+ glossary[word])
