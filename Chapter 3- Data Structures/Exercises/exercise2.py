9d86afd443ac010aa6a7a01afaccc77043e1f0c6
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:27:00 +0400

    Create exercise2.py
    exercise 2

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,8 @@
+names = ["Amber","Isra","Samra","Ayesha",]
+
+message = "Hi, Hope you all are well,{}!"
+
+print(message.format(names[0]))
+print(message.format(names[1]))
+print(message.format(names[2]))
+print(message.format(names[3]))
