e00c0d0c45c3c22eb4cc2bde92c61af60fad9dcc
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:31:11 +0400

    Create exercise3.py
    exercise3

---
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """Summarize the shirt that was made"""
+    print(f"Made a {size} t-shirt.")
+    print(f"Printed '{message}' on the front.")
+
+make_shirt('Medium', 'Hello')
+make_shirt(size='large', message='Good vibes')
