64840958bdcf6308579f76b9b5c1ef7b026b35b7
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:27:16 +0400

    Create exercise4.py
    exercise 4

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,17 @@
+guests = ["Amber","Isra","Samra","Ayesha"]
+
+print("Dear " + guests[0]+ ",")
+print("I am hosting a dinner event at my home, hope to see you.")
+
+print("/nDear "+ guests[1]+",")
+print("I am hosting a dinner event at my home, hope to see you.")
+
+print("/nDear "+ guests[2]+",")
+print("I am hosting a dinner event at my home, hope to see you.")
+
+print("/nDear "+ guests[3]+",")
+print("I am hosting a dinner event at my home, hope to see you.")
+
+
+
+
