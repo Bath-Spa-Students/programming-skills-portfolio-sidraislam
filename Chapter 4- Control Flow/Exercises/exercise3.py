301dbe20e39f70e6a3fe9a1faac94ded5832c64b
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:28:26 +0400

    Create exercise3.py
    exercise 3

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,29 @@
+# Green alien
+alien_color = "green"
+
+if alien_color == "green":
+    print("Earned 5 points.")
+elif alien_color == "yellow":
+    print("Earned 10 points.")
+else:
+    print("Earned 15 points.")
+
+# Yellow alien
+alien_color = "yellow"
+
+if alien_color == "green":
+    print("Earned 5 points.")
+elif alien_color == "yellow":
+    print("Earned 10 points.")
+else:
+    print("Earned 15 points.")
+
+# Red alien
+alien_color = "red"
+
+if alien_color == "green":
+    print("Earned 5 points.")
+elif alien_color == "yellow":
+    print("Earned 10 points.")
+else:
+    print("Earned 15 points.")
