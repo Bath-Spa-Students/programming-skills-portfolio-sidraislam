41c94e9d59d6203f744d5f436b46f3125d5a55b4
sidraislam <159785216+sidraislam@users.noreply.github.com>
2024-04-03 16:27:36 +0400

    Create exercise5.py
    exercise 5

---
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -0,0 +1,18 @@
+guests = ["Amber", "Isra", "Samra","Ayesha"]
+print("Dear"+ guests[0]+",You are invited to my dinner party.")
+print("Dear"+ guests[1]+",You are invited to my dinner party.")
+print("Dear"+ guests[2]+",You are invited to my dinner party.")
+print("Dear"+ guests[3]+",You are invited to my dinner party.")
+
+print("\nSorry,"+ guests[3]+"can not attend the party.")
+
+guests[3]= "Aliya"
+
+print("\nNew guest list:")
+print(guests)
+
+print("\nSending new invitations...")
+print("Dear"+ guests[0]+",you are invited to my dinner party.")
+print("Dear"+ guests[1]+",you are invited to my dinner party.")
+print("Dear"+ guests[2]+",you are invited to my dinner party.")
+print("Dear"+ guests[3]+",you are invited to my dinner party.")
